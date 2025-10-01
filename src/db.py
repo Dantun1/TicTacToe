9d4be878a5d9da0131e942abd47a6fcb2483ed34
Dantun1 <daniel@tunyan.com>
2025-10-01 15:54:40 +0100

    Add SQlite connection wrapper

---
+++ src/db.py
@@ -0,0 +1,50 @@
+import sqlite3
+from sqlite3 import Error
+from typing import ContextManager
+from pathlib import Path
+from contextlib import contextmanager
+
+DB_PATH = Path(__file__).parent.parent / "data" / "db.sqlite3"
+
+class Database:
+    """
+    Database wrapper class for facilitating connections.
+
+    Methods:
+        connect: Get a connection object to the database
+        get_connection: Context manager alternative for getting a connection object (recommended)
+    """
+
+    def __init__(self, db_path: Path = DB_PATH) -> None:
+        self.db_path = db_path
+
+    def connect(self) -> sqlite3.Connection:
+        """
+        Request a connection to the database
+
+        Returns:
+            sqlite3.Connection: connection to the sqlite3 database
+        """
+        try:
+            return sqlite3.connect(self.db_path)
+        except Error as e:
+            raise Error(f"Error connecting to database {e}")
+
+    @contextmanager
+    def get_connection(self) -> ContextManager[sqlite3.Connection]:
+        """
+        Get a connection to the database using a context manager
+
+        Yields:
+            sqlite3.Connection: connection to the sqlite3 database
+        Raises:
+            sqlite3.Error: If an error occurs while connecting or using the database.
+        """
+        try:
+            conn = self.connect()
+            yield conn
+        except Error as e:
+            raise Error(f"Error interacting with database {e}")
+        finally:
+            conn.close()
+
---
+++ tests/test_db.py
@@ -0,0 +1,27 @@
+import db
+import pytest
+
+
+@pytest.fixture
+def db_conn():
+    """
+    Instantiate a database wrapper object
+    """
+    return db.Database()
+
+
+def test_connect_with_context_manager(db_conn):
+    with db_conn.get_connection() as conn:
+        cursor = conn.cursor()
+        cursor.execute("SELECT 1;")
+        assert cursor.fetchone()[0] == 1
+
+def test_connect_as_connection_object(db_conn):
+    conn = db_conn.connect()
+    cursor = conn.cursor()
+    cursor.execute("SELECT 1;")
+    assert cursor.fetchone()[0] == 1
+    conn.close()
+
+
+
